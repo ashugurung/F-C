1e65afb353999f5b12d78d12f7eb0eca8e8d26db
ashugurung <82784557+ashugurung@users.noreply.github.com>
2022-08-11 00:32:42 +0100

    created Js

---
+++ myscript.js
@@ -0,0 +1,8 @@
+var button = document.querySelector('button');
+var box = document.getElementById('box');
+
+function changeColor(){
+    box.style.background = 'lightblue';
+    alert("Welcome to my website");
+}
+
